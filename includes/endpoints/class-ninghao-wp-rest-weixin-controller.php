b0986b6f13ec0713313894d7700efd2eef7b283e
wanghao8080 <117663444@qq.com>
2017-12-04 11:53:40 +0800

    在环境变量中存储小程序 ID 与 密钥

--- includes/endpoints/class-ninghao-wp-rest-weixin-controller.php
+++ includes/endpoints/class-ninghao-wp-rest-weixin-controller.php
@@ -65,8 +65,8 @@ class Ninghao_WP_REST_Weixin_Controller extends WP_REST_Controller {
 
   public function get_weixin_session( $js_code ) {
     $API_BASE = 'https://api.weixin.qq.com/sns/jscode2session';
-    $APP_ID = '';
-    $SECRET = '';
+    $APP_ID = env('WX_APP_ID');
+    $SECRET = env('WX_APP_SECRET');
     $url = "$API_BASE?appid=$APP_ID&secret=$SECRET&js_code=$js_code&grant_type=authorization_code";
 
     $response = wp_remote_get( $url );
